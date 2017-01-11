508838f68caadf52f8de94ebba865616b68f49b8
IvanLapshin <ivan.kliavakin@gmail.com>
2017-01-11 15:20:02 +0300

    after refactoring

--- main.js
+++ main.js
@@ -2,82 +2,84 @@ var SAVE_KEY = "save";
 var RED_COLOR = "#e74c3c";
 var BLUE_COLOR = "#27ae60";
 var PARENT_DIV = "arrayVisualization";
+var PARENT_DIV_ELEMENT;
 var EMPTY_STRING = "";
+var HIDDEN = "hidden";
 
 var sortState = {
-	currentArray : [],
-	outStep : 0,
-	inStep : 0
+  currentArray : [],
+  outStep : 0,
+  inStep : 0
 };
 
-
 function runSort() {
-	(function go() {
-    setTimeout(function () { 
-      nextSortStep();
-      if (document.getElementById('nextStep').style.visibility !== 'hidden') {
-        go();
-      }
-    }, 250);
-  })();
+  sortWithStepInterval(250);
 }
 
 function nextSortStep() {
-	var currentSortState = loadSortState();
-	var currentOutStep = currentSortState.outStep;
-	var currentInStep = currentSortState.inStep;
-	var currentElementStep = document.getElementById(currentInStep);
-	if (currentOutStep > 1) {
-		var currentInStepPlusOne = currentInStep + 1;
-		if (currentSortState.currentArray[currentInStep] > currentSortState.currentArray[currentInStepPlusOne]) {			
-			var child1 = currentElementStep;
-			var child2 = document.getElementById(currentInStepPlusOne);
-			var parent = document.getElementById(PARENT_DIV);
-			parent.insertBefore(child2, child1);
-			child1.id = EMPTY_STRING + currentInStepPlusOne;
-			child2.id = EMPTY_STRING + currentInStep;
+  var currentSortState = loadSortState();
+  var currentOutStep = currentSortState.outStep;
+  var currentInStep = currentSortState.inStep;
+  var currentElementStep = document.getElementById(currentInStep);
 
-			var c = currentSortState.currentArray[currentInStep];
-			currentSortState.currentArray[currentInStep] = currentSortState.currentArray[currentInStepPlusOne];
-			currentSortState.currentArray[currentInStepPlusOne] = c;
-		}
-		if ( ++currentInStep >= currentOutStep) {
-			currentInStep = 0;
-			currentOutStep--;
-		}
-		currentElementStep.style.backgroundColor = BLUE_COLOR
-		document.getElementById(currentInStep).style.backgroundColor = RED_COLOR;
-		currentSortState.inStep = currentInStep;
-		currentSortState.outStep = currentOutStep;
-	} else { 
-	  document.getElementById('nextStep').style.visibility = 'hidden';
-	  document.getElementById('runSort').style.visibility = 'hidden';
-	}
-	saveSortState(currentSortState);
+  if (currentOutStep < 2) {
+    document.getElementById('nextStep').style.visibility = HIDDEN;
+    document.getElementById('runSort').style.visibility = HIDDEN;
+    return;
+  }  
+
+  var currentInStepPlusOne = currentInStep + 1;
+  if (currentSortState.currentArray[currentInStep] > currentSortState.currentArray[currentInStepPlusOne]) {     
+    exchangeElements(currentElementStep, currentInStep, currentInStepPlusOne, currentSortState);
+  }
+
+  if ( ++currentInStep >= currentOutStep) {
+    currentInStep = 0;
+    currentOutStep--;
+  }
+
+  currentElementStep.style.backgroundColor = BLUE_COLOR
+  document.getElementById(currentInStep).style.backgroundColor = RED_COLOR;
+  
+  currentSortState.inStep = currentInStep;
+  currentSortState.outStep = currentOutStep;
+  saveSortState(currentSortState);
+}
+
+function exchangeElements(currentElementStep, currentInStep, currentInStepPlusOne, currentSortState) {
+  var child1 = currentElementStep;
+  var child2 = document.getElementById(currentInStepPlusOne);
+  PARENT_DIV_ELEMENT.insertBefore(child2, child1);
+  child1.id = EMPTY_STRING + currentInStepPlusOne;
+  child2.id = EMPTY_STRING + currentInStep;
+
+  var c = currentSortState.currentArray[currentInStep];
+  currentSortState.currentArray[currentInStep] = currentSortState.currentArray[currentInStepPlusOne];
+  currentSortState.currentArray[currentInStepPlusOne] = c;
 }
 
 function initArray() {
   var array = randomMassiveForSort();
-	createArrayVisualizationOnPage(array);
-	sortState.currentArray = array;
-	sortState.outStep = array.length;
-	document.getElementById("0").style.backgroundColor = RED_COLOR;
-	saveSortState(sortState);
+
+  PARENT_DIV_ELEMENT = document.getElementById(PARENT_DIV);
+  createArrayVisualizationOnPage(array);
+  
+  sortState.currentArray = array;
+  sortState.outStep = array.length;
+  saveSortState(sortState);
 }
 
 function createArrayVisualizationOnPage(array) {
-	var block;
-	var node;
-	for (var i = 0; i < array.length; i++) {
-		block = document.createElement("div");
-		block.id = EMPTY_STRING + i;
-		block.style.height = array[i] + "0px";
-		node = document.createTextNode(array[i]);
-		block.appendChild(node);
-		block.classList.add("column");
-		var parentDiv = document.getElementById(PARENT_DIV);
-		parentDiv.appendChild(block);
-	}
+  var block;
+  for (var i = 0; i < array.length; i++) {
+    block = document.createElement("div");
+    block.id = EMPTY_STRING + i;
+    block.style.height = array[i] + "0px";
+    block.innerHTML = array[i];
+    block.classList.add("column");
+    PARENT_DIV_ELEMENT.appendChild(block);
+  }
+  document.getElementById("0").style.backgroundColor = RED_COLOR;
 }
 
 function randomMassiveForSort() {
@@ -93,10 +95,17 @@ function randomMassiveForSort() {
   return result;
 }
 
+function sortWithStepInterval(interval) {
+  setTimeout(function () { 
+    nextSortStep();
+    if (document.getElementById('nextStep').style.visibility !== HIDDEN) {
+      sortWithStepInterval(interval);
+    }
+  }, interval);
+}
+
 function randomIntegerFromZero(max) {
-  var rand =  Math.random() * (max + 1) - 0.5;
-  rand = Math.round(rand);
-  return rand;
+  return Math.round(Math.random() * (max + 1) - 0.5);
 }
 
 function saveSortState(state) {
